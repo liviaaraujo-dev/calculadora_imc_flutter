a63012de0abfe99b68e1ec50344ff34d1f757e48
livia araujo <liviaaraujo.dev@gmail.com>
2023-07-01 15:39:41 -0300

    corrections

--- lib/pages/calculator_imc.dart
+++ lib/pages/calculator_imc.dart
@@ -1,9 +1,7 @@
 import 'package:calculadora_imc_flutter/model/imc_model.dart';
-import 'package:calculadora_imc_flutter/pages/calculator_imc.dart';
 import 'package:calculadora_imc_flutter/repository/imc_repository.dart';
 import 'package:calculadora_imc_flutter/shared/utils/util.dart';
 import 'package:flutter/material.dart';
-
 import '../shared/widgets/result_imc.dart';
 
 class CalculatorImc extends StatefulWidget {
@@ -17,11 +15,10 @@ class _CalculatorImcState extends State<CalculatorImc> {
 
   var altura = TextEditingController(text: "");
   var peso = TextEditingController(text: "");
-
   var imc;
   String classificacao = '';
 
-  final _imcRepository = ImcRepository();
+  final ImcRepository _imcRepository = ImcRepository();
   List<ImcModel> _imcs = <ImcModel>[];
 
   @override
@@ -33,10 +30,6 @@ class _CalculatorImcState extends State<CalculatorImc> {
   Future<void> _getImcs() async {
     _imcs = await _imcRepository.getData();
     setState(() {});
-    for(var i = 0; i < _imcs.length; i++){
-      print('${_imcs[i].id} ${_imcs[i].weight} ${_imcs[i].height} ${_imcs[i].imc} ${_imcs[i].classification}');
-    }
-    debugPrint(_imcs[0].classification.toString());
   }
 
   @override
@@ -44,152 +37,119 @@ class _CalculatorImcState extends State<CalculatorImc> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(title: const Text('Calculadora Imc'), centerTitle: true),
-        body: Form(
-      child: Column(
-        children: [
-          const Text('Calcule o seu imc'),
-          Column(
-            children: [
-              const Text('Sua altura'),
-              TextFormField(
-                controller: altura,
-                onChanged: (value){
-                  debugPrint(value);
-                },
-              )
-            ],
-          ),
-          Column(
-            children: [
-              Container(child: const Text('Seu peso')),
-              TextFormField(
-                controller: peso,
-                onChanged: (value){
-                  debugPrint(value);
-                },
-              )
-            ],
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              if(altura.text.isNotEmpty && peso.text.isNotEmpty ){
-                setState(() {
-                  imc = calculationImc(peso.text, altura.text);
-                  classificacao = classificationImc(imc);
-                });
-                saveImc();
-                resultImc(context, imc, classificacao);
-              }else{
-                if(altura.text.isEmpty && peso.text.isNotEmpty){
+        body: SingleChildScrollView(
+          child: Form(
+              child: Column(
+          children: [
+            const Text('Calcule o seu imc'),
+            Column(
+              children: [
+                const Text('Sua altura'),
+                TextFormField(
+                  controller: altura,
+                  onChanged: (value){
+                    debugPrint(value);
+                  },
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Container(child: const Text('Seu peso')),
+                TextFormField(
+                  controller: peso,
+                  onChanged: (value){
+                    debugPrint(value);
+                  },
+                )
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if(altura.text.isNotEmpty && peso.text.isNotEmpty ){
+                  setState(() {
+                    imc = calculationImc(peso.text, altura.text);
+                    classificacao = classificationImc(imc);
+                  });
+                  saveImc();
+                  resultImc(context, imc, classificacao);
+                  _getImcs();
+                }else{
+                  if(altura.text.isEmpty && peso.text.isNotEmpty){
+                    ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Informe a altura")));
+                    return;
+                  }
+                  
+                  if(peso.text.isEmpty && altura.text.isNotEmpty){
+                    ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Informe o peso")));
+                  return;
+                  }
+        
                   ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("Informe a altura")));
+                    const SnackBar(
+                      content: Text("Informe o peso e altura")));
                   return;
                 }
-                
-                if(peso.text.isEmpty && altura.text.isNotEmpty){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("Informe o peso")));
-                return;
-                }
-
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text("Informe o peso e altura")));
-                return;
-              }
-            }, 
-
-          child: Text('Calcular'),
-          ),
-          SingleChildScrollView(
-            child: Container(
-              child: SizedBox(
-              height: 300,
-                  child: ListView.builder(
-                    itemCount: _imcs.length,
-                    itemBuilder: (BuildContext context, index) {
-                      ImcModel _imc = _imcs[index];
-                      return Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Text(_imc.id.toString()),
-                            Text(_imc.weight.toString()),
-                            Text(_imc.height.toString()),
-                            Text(_imc.imc.toString()),
-                            Text(_imc.date.toString()),
-                            IconButton(icon: Icon(Icons.recycling), onPressed: (){
-                              _imcRepository.delete(int.parse(_imc.id.toString()));
-                            },)
-                          ],
-                        )
-                      );
-                    }
-                  
-                  )
+              }, 
+        
+            child: Text('Calcular'),
+            ),
+            Text('Imcs Calculados'),
+            SingleChildScrollView(
+              child: Container(
+                child: SizedBox(
+                height: 300,
+                    child: ListView.builder(
+                      itemCount: _imcs.length,
+                      itemBuilder: (BuildContext context, index) {
+                        ImcModel _imc = _imcs[index];
+                        return Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(_imc.date.toString()),
+                              Text(_imc.classification.toString()),
+                              Text(_imc.imc.toString()),
+                              Text(_imc.weight.toString() + ' kg'),
+                              Text(_imc.height.toString() + ' m'),
+                              IconButton(icon: Icon(Icons.recycling), onPressed: (){
+                                _imcRepository.delete(int.parse(_imc.id.toString()));
+                                _getImcs();
+                              },)
+                            ],
+                          )
+                        );
+                      }
+                    
+                    )
+                ),
+              ),
+            )
+          ],
               ),
             ),
-          )
-        ],
-      ),
-    )
+        )
       )
     );
-
-    
   }
 
-  // void calculeImc(){
-  //   setState(() {
-  //     imc = (double.parse(peso.text) / (double.parse(altura.text) * double.parse(altura.text))).toStringAsFixed(2);
-  //     imc = double.parse(imc);
-  //   });
-  // }
-
-  // void  classificacaoImc(){
-  //   if(imc < 16){
-  //     classificacao = "Magreza extrema";
-  //   }else if(imc >= 16 && imc < 17){
-  //     classificacao = "Magreza moderada";
-  //   }else if(imc >= 17 && imc < 18.5){
-  //     classificacao = "Magreza leve";
-  //   }else if(imc >= 18.5&& imc < 25){
-  //     classificacao = "Saudável";
-  //   }else if(imc >= 25 && imc < 30){
-  //     classificacao = "Sobrepeso";
-  //   }else if(imc >= 30 && imc < 35){
-  //     classificacao = "Obesidade grau I";
-  //   }else if(imc >= 35 && imc < 40){
-  //     classificacao = "Obesidade grau II(severa)";
-  //   }else{
-  //     classificacao = "Obesidade grau III(mórbida)";
-  //   }
-  //   setState(() {});
-  // }
-
   saveImc() async {
-    // var imcF = const ImcModel(
-    //   weight: double.parse(peso.text),
-    //   height: double.parse(altura.text),
-    //   imc: imc,
-    //   classification: classificacao
-    // );
     final now = DateTime.now();
-    print('${now.day}/${now.month}/${now.year}');
+    String day = now.day.toString().length == 1 ? '0'+ now.day.toString() : now.day.toString();
+    String month = now.month.toString().length == 1 ? '0'+ now.month.toString() : now.month.toString();
 
     await _imcRepository.create(
       ImcModel(
-      weight: double.parse(peso.text),
-      height: double.parse(altura.text),
-      imc: imc,
-      classification: classificacao,
-      date: '${now.day}/${now.month}/${now.year}'
-    )
+        weight: double.parse(peso.text),
+        height: double.parse(altura.text),
+        imc: imc,
+        classification: classificacao,
+        date: '${day}/${month}/${now.year}'
+      )
     );
   }
-
-
-
 }
